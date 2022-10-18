979674a633afabffc925ccaa14a1547d00443684
Noelia Cruz <112984139+NoeliaCush@users.noreply.github.com>
2022-10-18 18:53:03 -0300

    Add files via upload

---
+++ Guia06POOEjAprendizaje02/src/guia06pooejaprendizaje02/Guia06POOEjAprendizaje02.java
@@ -0,0 +1,17 @@
+package guia06pooejaprendizaje02;
+
+import guia06pooejaprendizaje02.Entidad.Circunferencia;
+
+public class Guia06POOEjAprendizaje02 {
+
+    public static void main(String[] args) {
+            
+            Circunferencia c1 = new Circunferencia();
+         
+            c1.crearCircunferencia();
+            c1.calcularArea();
+            c1.calcularPerimetro();
+            
+    }
+
+}
